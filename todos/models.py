7ed71e413318c9e55f278a4dd5bab025cb1ff6a1
Mohamad anas fattoum <samaanas30@gmail.com>
2024-07-16 18:13:23 +0200

    create models

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Task(models.Model):
+    auther = models.ForeignKey(User, related_name='task_user', on_delete=models.CASCADE, null=True, blank=True)
+    task = models.CharField(max_length=100)
+    task_text = models.TextField(max_length=1000)
+    date = models.DateTimeField(default=timezone.now)
+    complate = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.task
+
+    class Meta:
+        order_with_respect_to = 'auther'
